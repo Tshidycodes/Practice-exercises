98c1533ce524de23ea88d0b6667e567248eb41c1
Tshidy <hlaleletshidy@gmail.com>
2024-05-21 08:12:26 +0200

    added a function that prints square in hash chars

--- square.js
+++ square.js
@@ -1,1 +1,31 @@
-﻿
+﻿//Write a function square. It takes in an integer and then prints out a square using the hash character.
+
+/*For example, square(2) should print:
+
+##
+##
+For example, square(4) should print:
+
+####
+####
+####
+#### 
+
+///////
+*/
+function square(n) {
+  //create a line string with hash characters, Initialize line as empty string
+  //Use a for loop FROM 0 TO n-1, APPEND "#" to line
+  //Print the line string n times
+  //Use for loop to print from 0 TO n-1: PRINT LINE
+
+  let line = "";
+  for (let i = 0; i < n; i++) {
+    line += "#";
+  }
+  //Print the line n times
+  for (let i = 0; i < n; i++) {
+    console.log(line); //0 & 1 means two iterations so this code is run two times
+  }
+}
+square(2);
