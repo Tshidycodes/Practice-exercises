c8f7f904623f956ba6793a60149e5448e56db55a
Tshidy <hlaleletshidy@gmail.com>
2024-05-23 21:31:15 +0200

    created a function that sums all Numbers in a range

---
+++ addRange.js
@@ -0,0 +1,24 @@
+﻿//We'll pass you an array of two numbers. Return the sum of those two numbers and the all the numbers between them
+//Create a function that sums All Numbers in a range.
+//RANGE - first find the minimum and maximum values
+//FOR loop start at minValue (let i = minValue) end at maxValue i<= maxvalue, increment
+//initialize variable at O to store the cummulative values being added
+//add the values as they are being looped through to the total
+//return the sum
+function sumAll(arr) {
+  let minValue = Math.min(arr[0], arr[1]);
+  let maxValue = Math.max(arr[0], arr[1]);
+
+  let sum = 0;
+  for (let i = minValue; i <= maxValue; i++) {
+    sum += i;
+  }
+  console.log(sum);
+}
+sumAll([1, 4]);
+
+//you can also find max number by using spread operator
+/* let numbers = [3,4,1,6,7,8,9,2];
+let max = Math.max(...numbers);
+console.log(max); outputs: 8;
+*/
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 ﻿//Create a function that "claps" 5 times
+/*
 function clap(n) {
   for (let i = 0; i < n; i++) {
-    console.log("clap👋");
+    //console.log("clap👋");
   }
 }
 
@@ -16,6 +17,32 @@ function clapOnRow(n) {
   for (let i = 0; i < n; i++) {
     clapLine += "👋";
   }
-  console.log(clapLine);
+  //console.log(clapLine);
 }
 clapOnRow(5);
+
+//How to loop through an array
+
+let fruits = ["apple", "lemon", "pear"];
+console.log(fruits[0]); //means get me an element at the zero index
+//lets append:
+let favouriteFruit = "";
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]); //simply means first iter i= 0 therefore fruits[0]; then i= 1 therefore fruits[1]; then i = 2 so fruits[2]
+  favouriteFruit += fruits[i] + " ";
+  console.log(favouriteFruit);
+}
+*/
+
+// FOR LOOP to run through an object because objects do not have indexes or order and are accessed through their properties:
+
+let data = { name: "John", age: 34, maritalStatus: true };
+console.log(data["maritalStatus"]);
+for (let element in data) {
+  //console.log(element); //goes through all the properties
+  console.log(element, data[element]);
+}
+
+//Nested Loops and 2-Dimentional  Arrays
+
+let numberGrid = [[1, 2, 3], [4.5, 6], [7, 8, 9], [0]];
