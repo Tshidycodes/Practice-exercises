f221f7df09f1298486c6d6e21ade9f9118bed4f1
Tshidy <hlaleletshidy@gmail.com>
2024-06-06 21:40:20 +0200

    organised folders

---
+++ coderbyte/Username_Validation.js
@@ -0,0 +1,24 @@
+﻿function CodelandUsernameValidation(str) {
+  // Rule 1: Check length
+  if (str.length < 4 || str.length > 25) {
+    return "false";
+  }
+
+  // Rule 2: Check if the first character is a letter
+  if (!/^[a-zA-Z]/.test(str)) {
+    return "false";
+  }
+
+  // Rule 3: Check if the username contains only letters, numbers, and underscores
+  if (!/^[a-zA-Z0-9_]+$/.test(str)) {
+    return "false";
+  }
+
+  // Rule 4: Check if the username ends with an underscore
+  if (str.endsWith("_")) {
+    return "false";
+  }
+
+  // If all conditions are met, return true
+  return "true";
+}
---
+++ coderbyte/secondLargestNun.js
@@ -0,0 +1,38 @@
+﻿//Question 1 - Second Largest Number
+//Given an array of size N, print second largest
+// distinct element from the array
+//input: [12, 35,1,10,34,1] --->> output:34
+//input: [10,5,10] --->> output:5
+//first remove values that are repeating, show unique values -use Set
+//convert set into array
+// sort the values
+//then get the index
+
+function SecondLargest(arr) {
+  const uniqueArr = Array.from(new Set(arr));
+  uniqueArr.sort((a, b) => {
+    return b - a;
+  });
+  if (uniqueArr.length >= 2) {
+    return uniqueArr[1];
+  } else {
+    return -1;
+  }
+}
+console.log(SecondLargest([5]));
+
+//Optimised Approach
+function SecondLargestOptimised(arr) {
+  let largest = Number.NEGATIVE_INFINITY;
+  let secondLargest = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] != largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+console.log(SecondLargestOptimised([14, 23, 65, 45]));
