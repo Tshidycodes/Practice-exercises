53f1ca7232413b0387ebc8841d9e9eefb2312400
Tshidy <hlaleletshidy@gmail.com>
2024-07-19 08:51:28 +0200

    19 July 2024

---
+++ FreeCodeCamp/truncate2.js
@@ -0,0 +1,9 @@
+﻿/* Truncate a tring(first argument) if it is longer than the maximum string length (second argument). Return the truncated string with a ... ending */
+
+function truncateString(str, num) {
+  if (str.length <= num) return str;
+  else {
+    return str.slice(0, num) + "...";
+  }
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
