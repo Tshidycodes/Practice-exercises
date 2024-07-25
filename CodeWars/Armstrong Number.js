f84f7ee7a597bbce78c4c3cca4e9add0d7e1bfbd
Tshidy <hlaleletshidy@gmail.com>
2024-07-25 18:07:18 +0200

    25 July 2024

---
+++ CodeWars/Armstrong Number.js
@@ -0,0 +1,22 @@
+﻿/*
+A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+For example, take 153 (3 digits), which is narcissistic:
+
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+and 1652 (4 digits), which isn't:
+
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+*/
+
+function narcissistic(value) {
+  const digits = value.toString();
+  const powerDigit = digits.length;
+  let sum = 0;
+  for (i = 0; i < digits.length; i++) {
+    sum += Math.pow(digits[i], powerDigit);
+  }
+
+  return sum === value;
+}
+console.log(narcissistic(1938));
