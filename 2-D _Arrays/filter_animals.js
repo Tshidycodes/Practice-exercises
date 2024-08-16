8552d8dd25dd800b6a6a79978d14690a604344f5
Tshidy <hlaleletshidy@gmail.com>
2024-08-16 22:08:58 +0200

    16 August 2024

---
+++ 2-D _Arrays/filter_animals.js
@@ -0,0 +1,21 @@
+﻿//Array of animals where each element is an object representing an animal
+const animals = [
+  { name: "Lion", family: "Cat", food: "Meat" },
+  { name: "Tiger", family: "Cat", food: "Meat" },
+  { name: "shark", family: "Fish", food: "Meat" },
+  { name: "sparrow", family: "Bird", food: "Grass" },
+  { name: "eagle", family: "Bird", food: "Meat" },
+  { name: "penguin", family: "Bird", food: "fish" },
+  { name: "squirrel", family: "rodent", food: "nuts" },
+];
+
+//we want to filter our animals based on some conditions
+
+const meatEaters = animals.filter((animals) => animals.food === "Meat");
+const grassEaters = animals.filter((animals) => animals.food === "Grass");
+const catOmnivores = animals.filter(
+  (animals) => animals.family === "Cat" && animals.food === "Meat"
+);
+console.log(meatEaters);
+console.log(grassEaters);
+console.log(catOmnivores);
---
+++ 2-D _Arrays/filter_method.js
@@ -0,0 +1,17 @@
+﻿const ages = [32, 33, 16, 40, 34, 21, 67, 46, 99, 20];
+//filtering adults manually:
+const adults = [];
+for (let i = 0; i < ages.length; i++) {
+  if (ages[i] >= 18) {
+    adults.push(ages[i]);
+  }
+}
+console.log(adults);
+
+//Using filter method: used to filter array elements
+const numberz = [32, 33, 16, 40, 34, 21, 67, 46, 99, 20];
+// replace annonymous function with arrow function:
+//const faveNumbers = numberz.filter(function (numberz) {
+//return numberz > 18;
+const faveNumbers = numberz.filter((numberz) => numberz > 18);
+console.log(faveNumbers);
