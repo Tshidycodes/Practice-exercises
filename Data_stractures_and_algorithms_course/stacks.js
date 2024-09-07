5ecb565d129c8191bb1e24a7143e0b2c3fcb9481
Tshidy <hlaleletshidy@gmail.com>
2024-09-07 19:01:07 +0200

    07 September 2024

---
+++ Data_stractures_and_algorithms_course/stacks.js
@@ -0,0 +1,34 @@
+﻿//stacks - last in, first out, uses methods: push,pop,peek,length
+
+//makeApalindrome: words spelled the same backward and forwards
+
+function palindrome(word) {
+  let letters = [];
+  let reversed = "";
+
+  //put letters of word into a stack
+  for (let i = 0; i < word.length; i++) {
+    letters.push(word[i]);
+  }
+  //pop off the stack in reverse order
+  for (let i = 0; i < word.length; i++) {
+    reversed += letters.pop();
+  }
+  if (reversed === word) {
+    return `${word} is a palindrome`;
+  } else {
+    return `${word} is not a palindrome`;
+  }
+}
+console.log(palindrome("racecar"));
+console.log(palindrome("hen"));
+
+function reverseString(s) {
+  let arr = s.split("");
+  let reversed = "";
+  for (let i = 0; i < s.length; i++) {
+    reversed += arr.pop(i);
+  }
+  return reversed;
+}
+console.log(reverseString("hello"));
